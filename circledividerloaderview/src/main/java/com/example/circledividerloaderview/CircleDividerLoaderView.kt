534dc7b7b3278a4999445a02029b6495a86a83e3
Anwesh43 <anweshthecool0@gmail.com>
2020-12-19 04:08:40 +0530

    adding Canvas extension methods

--- circledividerloaderview/src/main/java/com/example/circledividerloaderview/CircleDividerLoaderView.kt
+++ circledividerloaderview/src/main/java/com/example/circledividerloaderview/CircleDividerLoaderView.kt
@@ -31,3 +31,37 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawCircleDividerLoader(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val size : Float = Math.min(w, h) / sizeFactor
+    save()
+    translate(w / 2, h / 2)
+    save()
+    translate(0f, -h / 4 - size)
+    drawArc(RectF(-size, -size, size, size), 360f * sc2, 360f * (sc1 - sc2), false, paint)
+    restore()
+    save()
+    translate(0f, h / 4 - size)
+    for (j in 0..(lines - 1)) {
+        val sc1k : Float = sc1.divideScale(j, lines)
+        val sc2k : Float = sc2.divideScale(j, lines)
+        save()
+        rotate((deg / lines) * j)
+        drawLine(size * sc2k, 0f, size * sc1k, 0f, paint)
+        restore()
+    }
+    restore()
+    restore()
+}
+
+fun Canvas.drawCDLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.style = Paint.Style.STROKE
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawCircleDividerLoader(scale, w, h, paint)
+}
