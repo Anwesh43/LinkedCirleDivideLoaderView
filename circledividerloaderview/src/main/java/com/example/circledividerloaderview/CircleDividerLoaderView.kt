838789ec73fd46f741671dbfeec3edc38866366a
Anwesh43 <anweshthecool0@gmail.com>
2020-12-19 04:35:26 +0530

    adding Renderer class

--- circledividerloaderview/src/main/java/com/example/circledividerloaderview/CircleDividerLoaderView.kt
+++ circledividerloaderview/src/main/java/com/example/circledividerloaderview/CircleDividerLoaderView.kt
@@ -192,4 +192,27 @@ class CircleDividerLoaderView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : CircleDividerLoaderView) {
+
+        private val animator : Animator = Animator(view)
+        private val cdl : CircleDivideLoader = CircleDivideLoader(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            cdl.draw(canvas, paint)
+            animator.animate {
+                cdl.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            cdl.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
