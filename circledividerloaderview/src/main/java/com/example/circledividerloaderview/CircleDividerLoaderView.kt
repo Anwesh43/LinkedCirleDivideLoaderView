b61e04c945661f34d1e42f78ab7d1766b36a2be4
Anwesh43 <anweshthecool0@gmail.com>
2020-12-19 03:54:40 +0530

    adding constants and libraries

--- circledividerloaderview/src/main/java/com/example/circledividerloaderview/CircleDividerLoaderView.kt
+++ circledividerloaderview/src/main/java/com/example/circledividerloaderview/CircleDividerLoaderView.kt
@@ -1,2 +1,28 @@
 package com.example.circledividerloaderview
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val parts : Int = 2
+val lines : Int = 6
+val scGap : Float = 0.02f / lines
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 5.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val deg : Float = 360f
+val colors : Array<Int> = arrayOf(
+    "#F44336",
+    "#3F51B5",
+    "#2196F3",
+    "#FF9800",
+    "#795548"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
