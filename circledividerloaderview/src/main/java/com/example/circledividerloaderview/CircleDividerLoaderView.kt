357d9a0bba5739bd7536b452787191e28c742e57
Anwesh43 <anweshthecool0@gmail.com>
2020-12-19 04:11:14 +0530

    adding CircleDividerLoaderView class

--- circledividerloaderview/src/main/java/com/example/circledividerloaderview/CircleDividerLoaderView.kt
+++ circledividerloaderview/src/main/java/com/example/circledividerloaderview/CircleDividerLoaderView.kt
@@ -65,3 +65,19 @@ fun Canvas.drawCDLNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawCircleDividerLoader(scale, w, h, paint)
 }
+
+class CircleDividerLoaderView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
