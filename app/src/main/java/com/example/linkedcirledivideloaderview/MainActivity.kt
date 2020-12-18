8bb93621c89788462e374adec29b7b7648cf88dc
Anwesh43 <anweshthecool0@gmail.com>
2020-12-19 04:41:10 +0530

    adding view in activity and fullscreen support

--- app/src/main/java/com/example/linkedcirledivideloaderview/MainActivity.kt
+++ app/src/main/java/com/example/linkedcirledivideloaderview/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.linkedcirledivideloaderview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.circledividerloaderview.CircleDividerLoaderView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        CircleDividerLoaderView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
